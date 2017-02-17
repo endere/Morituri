11877fd9f94a2eefe8337152085c07b19c74a8ec
Erik Enderlein <erik.end@gmail.com>
2017-02-17 12:51:28 -0600

    added two weapon dice, plus functionality to roll any of their unique sides

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 'use strict';
-
 function createElement(tagType, tagIdentifier, tagIdentifiername, elementContent, sectionId){
   var element = document.createElement(tagType);
   element.setAttribute(tagIdentifier, tagIdentifiername);
@@ -9,25 +8,37 @@ function createElement(tagType, tagIdentifier, tagIdentifiername, elementContent
   sectionId.appendChild(element);
   //this element creation function created by Benjamin Ayzenberg.
 }
-function CreateSchool(attack, defense, evasion, diceOne, diceTwo){
+function School(attack, defense, evasion, diceOne, diceTwo){
   this.attack = attack;
   this.defesnse = defense;
   this.evasion = evasion;
   this.diceOne = diceOne;
   this.diceTwo = diceTwo;
 };
-function CreateDice(){
+function Dice(name,sideOne,sideTwo,sideThree,sideFour,sideFive,sideSix){
+  this.name = name,
+  this.sideOne = sideOne,
+  this.sideTwo = sideTwo,
+  this.sideThree = sideThree,
+  this.sideFour = sideFour,
+  this.sideFive = sideFive,
+  this.sideSix = sideSix,
+  this.sides = [sideOne,sideTwo,sideThree,sideFour,sideFive,sideSix];
+};
+
+function Item(){
 
 };
 
-function CreateItem(){
-
-};
-
-function CreateGladiator(){
+function Gladiator(){
   this.name = name;
   this.school = school;
   this.item = item;
+};
+Dice.prototype.rollDice = function(){
+  var value = roll(1) - 1;
+  console.log(this.sides[value]);
+  return this.sides[value];
 };
 // CreateDice.prototype.roll = function
 function roll(amount){
@@ -58,4 +69,8 @@ function gatherData(amount){
 //for (var j = 0; j < sides.length; j ++){
 
 //}
-gatherData(1000);
+gatherData(10000);
+var sword = new Dice('sword','blank','defense','defense','attack','attack',['attack','defense']);
+var shield = new Dice('shield','blank','evasion','attack','defense','defense',['defense','defense']);
+console.log('the sword rolled ' + sword.rollDice());
+console.log('the shield rolled ' + shield.rollDice());
